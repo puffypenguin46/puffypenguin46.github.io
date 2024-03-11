597b7cc0c76d3a98d20aa33413557d1b1e4cac0f
puffypenguin46 <161518195+puffypenguin46@users.noreply.github.com>
2024-03-11 14:07:14 -0600

    nn

--- scene/sketch.js
+++ scene/sketch.js
@@ -6,30 +6,79 @@
 // - describe what you did to take this project "above and beyond"
 
 
-let x;
-let y;
-let sanya;
 
-function preload() {
-  sanya = createImg('sanya.png')
-}
+// universal variables 
 
-function preload() {
-  up = createImg('up.png')
-}
+// up and down arrows
+
+
+let x = 0
+
+let y = 0
+
+let r = 0;
+
+let g = 0;
+
+let b = 0;
+
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(400, 400);
+}
+
+
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    if (r <= 255) {
+      r += 10
+    }
+    else 
+      r = r
+  } else if (keyCode === LEFT_ARROW) {
+    if (b <= 255) {
+      b += 10
+    } 
+    else 
+      b = b
+  } else if (keyCode === RIGHT_ARROW) {
+    if (g <= 255) {
+      g += 10
+    } 
+    else 
+      g = g
+  }
 }
 
 function draw() {
-  background(220);
-  sanya.position(900, 200);
-  sanya.size(windowWidth / 4, windowHeight / 4);
-
-  up.position(200, 100);
-  up.size(windowWidth / 8, windowHeight / 8);
+  background(150, 230, 240);
+  x += (mouseX - x) * 0.03; 
+  y += (mouseY - y) * 0.03; 
+  keyPressed(); {
+    if (keyCode === UP_ARROW) {
+      if (r <= 255) {
+        r += 10
+    }
+      else 
+      r = r
+  } else if (keyCode === LEFT_ARROW) {
+    if (b <= 255) {
+      b += 10
+    } 
+    else 
+      b = b
+  } else if (keyCode === RIGHT_ARROW) {
+    if (g <= 255) {
+      g += 10
+    } 
+    else 
+      g = g
+  }
+  }
+  fill(r, g, b)
+  quad(x, y, 70, 230, 220, 70, 230, 60);
 }
+
 
 
 
